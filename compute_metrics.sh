1a559905e7eca1546f493980f2fdf94fb44917da
ckevar <ckevar@gmail.com>
2025-09-16 10:57:42 +0200

    modified:   compute_metrics.sh 	modified:   trackeval/datasets/waymov2mot_2d_box_fairmot_multiclass.py

--- compute_metrics.sh
+++ compute_metrics.sh
@@ -1,24 +1,28 @@
-eval_fairmot_mot17() {
+eval_mot17() {
+  EXPERIMENT_DIR="$1"
   python3 scripts/run_mot_challenge_fairmot_multiclass.py \
     --BENCHMARK MOT17 \
     --GT_FOLDER ~/Documents/Datasets/mot/MOT17/train/ \
     --TRACKERS_FOLDER ~/Documents/Datasets/mot/MOT17/results/ \
+    --TRACKERS_TO_EVAL "$EXPERIMENT_DIR" \
     --SEQMAP_FOLDER data/gt/multiclass/seqmaps/ \
     --CLASSES_TO_EVAL 'pedestrian' 'person_on_vehicle' 'car' 'bicycle' 'motorbike' 'static_person' \
     --SKIP_SPLIT_FOL True
 }
 
-eval_fairmot_kitti() {
+eval_kitti() {
+  EXPERIMENT_DIR="$1"
   python3 scripts/run_kitti_fairmot_multiclass.py \
     --GT_FOLDER ~/Documents/Datasets/mot/KITTI/training/ \
     --TRACKERS_FOLDER ~/Documents/Datasets/mot/KITTI/training/results/ \
+    --TRACKERS_TO_EVAL "$EXPERIMENT_DIR" \
     --TRACKER_SUB_FOLDER '' \
     --SEQMAP_FOLDER data/gt/kitti/kitti_2d_box_train/ \
     --CLASSES_TO_EVAL 'car' 'van' 'truck' 'pedestrian' 'person' 'cyclist' 'tram' \
     --SPLIT_TO_EVAL 'training'
 }
 
-eval_fairmot_waymov2mot() {
+eval_waymov2mot() {
   EXPERIMENT_DIR="$1"
   python3 scripts/run_waymov2mot_fairmot_multiclass.py \
     --GT_FOLDER ~/Documents/Datasets/mot/waymo/waymoV2-mot17/train \
@@ -27,10 +31,31 @@ eval_fairmot_waymov2mot() {
     --SEQMAP_FILE data/gt/multiclass/seqmaps/waymoV2MOT-train.txt \
     --CLASSES_TO_EVAL 'car' 'pedestrian' 'bicycle'  \
     --SKIP_SPLIT_FOL True
-    #--SEQMAP_FOLDER data/gt/multiclass/seqmaps/ \
-    
 }
 
-#eval_fairmot_mot17
-#eval_fairmot_kitti
-eval_fairmot_waymov2mot "MOT17_test_public_dla34"
+#eval_mot17 "fairmot_mot17_baseline"
+#eval_kitti "fairmot_kitti_baseline"
+#eval_waymov2mot "fairmot_waymoV2Mot_baseline"
+#eval_mot17 "deepsort_mot17_baseline"
+#eval_kitti "deepsort_kitti_baseline"
+#eval_mot17 "deepsort_mot17_reid_tuned"
+#eval_kitti "deepsort_kitti_reid_tuned"
+#eval_mot17 "bytetrack_mot17_baseline"
+#eval_kitti "bytetrack_kitti_baseline"
+#eval_mot17 "c_bioutrack_mot17_baseline"
+#eval_kitti "c_bioutrack_kitti_baseline"
+#eval_waymov2mot "deepsort_waymoV2Mot_baseline"
+#eval_waymov2mot "bytetrack_waymoV2Mot_baseline"
+#eval_mot17 "ocsort_mot17_baseline"
+#eval_kitti "ocsort_kitti_baseline"
+#eval_waymov2mot "ocsort_waymoV2Mot_baseline"
+#eval_waymov2mot "c_bioutrack_waymoV2Mot_baseline"
+#eval_mot17 "ucmctrack_mot17_baseline"
+#eval_kitti "ucmctrack_kitti_baseline"
+#eval_waymov2mot "ucmctrack_waymoV2Mot_baseline"
+#eval_mot17 "fairmot_mot17_dla34_tuned"
+#eval_mot17 "fairmot_mot17_dla34_tuned_model30"
+#eval_kitti "fairmot_kitti_dla34_tuned"
+#eval_mot17 "boosttrack_mot17_baseline"
+eval_kitti "boosttrack_kitti_baseline"
+eval_waymov2mot "boosttrack_waymoV2Mot_baseline"
--- trackeval/datasets/waymov2mot_2d_box_fairmot_multiclass.py
+++ trackeval/datasets/waymov2mot_2d_box_fairmot_multiclass.py
@@ -66,32 +66,18 @@ class WaymoV2MotFairMOTMulticlass(_BaseDataset):
         self.output_sub_fol = self.config['OUTPUT_SUB_FOLDER']
 
         # Get classes to eval
-        self.valid_classes = ['pedestrian', 
-                              'person_on_vehicle', 
-                              'car', 
-                              'bicycle', 
-                              'motorbike', 
-                              'static_person',
-                              'crowd']
+        self.valid_classes = ['car', 
+                              'pedestrian', 
+                              'bicycle']
         
         self.class_list = [cls.lower() if cls.lower() in self.valid_classes else None
                            for cls in self.config['CLASSES_TO_EVAL']]
         if not all(self.class_list):
             raise TrackEvalException('Attempted to evaluate an invalid class. Only pedestrian class is valid.')
 
-        self.class_name_to_class_id = {'pedestrian': 1, 
-                                       'person_on_vehicle': 2, 
-                                       'car': 3, 
-                                       'bicycle': 4, 
-                                       'motorbike': 5,
-                                       'non_mot_vehicle': 6, 
-                                       'static_person': 7, 
-                                       'distractor': 8, 
-                                       'occluder': 9,
-                                       'occluder_on_ground': 10, 
-                                       'occluder_full': 11, 
-                                       'reflection': 12, 
-                                       'crowd': 13}
+        self.class_name_to_class_id = {'car': 1, 
+                                       'pedestrian': 2, 
+                                       'bicycle': 3}
 
         self.valid_class_numbers = list(self.class_name_to_class_id.values())
 
@@ -360,7 +346,7 @@ class WaymoV2MotFairMOTMulticlass(_BaseDataset):
         """ Legacy
         distractor_class_names = ['person_on_vehicle', 'static_person', 'distractor', 'reflection']
         """
-        distractor_class_names = ['distractor', 'reflection', 'non_mot_vehicle']
+        distractor_class_names = []
         #FIXME: Remove me - if self.benchmark == 'MOT20':
         #    distractor_class_names.append('non_mot_vehicle')
         distractor_classes = [self.class_name_to_class_id[x] for x in distractor_class_names]
